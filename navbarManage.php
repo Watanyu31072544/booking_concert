3215fff2af54164916bda34c9f796d26ee7d1ee3
Watanyu31072544 <kh.watanyu_st@tni.ac.th>
2024-02-01 13:35:32 +0700

    Update navbarManage.php

--- navbarManage.php
+++ navbarManage.php
@@ -1,7 +1,7 @@
 <?php
 require("dbconnect.php");
 ?>
-<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+<ul class="navbar-nav bg-gradient-success sidebar sidebar-dark accordion" id="accordionSidebar">
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="ManageConcert.php">    
         <div class="sidebar-brand-text">Manage Booking Concert</sup></div>
     </a>
