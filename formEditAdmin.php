ca33d594d03d89300c84e9201f1602c6f8a2752e
Watanyu31072544 <kh.watanyu_st@tni.ac.th>
2024-02-01 09:27:43 +0700

    Update formEditAdmin.php

--- formEditAdmin.php
+++ formEditAdmin.php
@@ -32,7 +32,7 @@
         <!-- Content Wrapper -->
         <div id="content-wrapper" class="d-flex flex-column">
             <!-- Main Content -->
-            <div id="content">
+            <div id="content" style="background-color: #B5DE0C;">
                 <?php
                 include('topbarManage.php');
                 ?>
