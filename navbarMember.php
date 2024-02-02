c26109a2584f2785ed076accd090e115202bd686
Watanyu31072544 <kh.watanyu_st@tni.ac.th>
2024-02-02 10:58:24 +0700

    Update navbarMember.php

--- navbarMember.php
+++ navbarMember.php
@@ -3,7 +3,7 @@ require("dbconnect.php");
 ?>
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="PageBookingConcert.php">        
-        <div class="sidebar-brand-text">Booking Concert</sup></div>
+        <div class="sidebar-brand-text">Booking Concert</div>
     </a>
     <hr class="sidebar-divider my-0">          
     <li class="nav-item active">
