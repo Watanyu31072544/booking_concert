4ef4b12b1ff52319d2b46bde0b38d958a871b789
Watanyu31072544 <kh.watanyu_st@tni.ac.th>
2024-02-02 10:58:33 +0700

    Update topbarMember.php

--- topbarMember.php
+++ topbarMember.php
@@ -3,6 +3,8 @@ session_start();
 require("dbconnect.php");
 ?>
 <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="PageBookingConcert.php">หน้าแรก</a>
         <ul class="navbar-nav ml-auto">
         <?php
             if (!$_SESSION["m_id"]) {
@@ -14,5 +16,6 @@ require("dbconnect.php");
         ?>
         <p style="color: black;"> สวัสดี <?php echo $member["m_fullname"];?></p>
         </ul>
-        <?php  } ?>
+        <?php  } ?>        
+    </div>
 </nav>
