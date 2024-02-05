7aed5492be40a854fd856e6b5b972ddd71cbea9b
Watanyu31072544 <kh.watanyu_st@tni.ac.th>
2024-02-05 14:18:54 +0700

    Update checkLoginManageConcert.php

--- checkLoginManageConcert.php
+++ checkLoginManageConcert.php
@@ -7,10 +7,10 @@
   if(isset($_GET['ad_username'])){
     $ad_username = $_GET['ad_username'];
     $ad_password = $_GET['ad_password'];
-
+    //เช็คชื่อผู้ใช้และรหัสผ่านของผู้ดูแลที่Adminเพิ่มสมาชิกไว้ก่อนหน้านี้
     $sql = "SELECT * FROM admin WHERE ad_username='".$_GET['ad_username']."' AND ad_password='".$_GET['ad_password']."'";
     $result = mysqli_query($db,$sql);
-    
+    //เช็คข้อมูลที่อยู่ในฐานข้อมูลหรือเปล่า ถ้าอยู่ในฐานข้อมูลจากการเพิ่มสมาชิกมาแล้ว สามารถเข้าสู่จัดการข้อมูลของคอนเสิร์ตได้
     if(mysqli_num_rows($result) == 1){
       $admin = mysqli_fetch_array($result);
       $_SESSION["ad_id"] = $admin["ad_id"];
@@ -21,6 +21,7 @@
         echo "<script> window.location='ManageConcert.php'; </script> "; 
       }     
     }
+    //ถ้าไม่ได้อยู่ในฐานข้อมูล Adminจะต้องทำการเพิ่มสมาชิกก่อนเท่านั้น ถึงจะเข้ามาจัดการข้อมูลของคอนเสิร์ตได้
     else{
       echo "Error:" . $sql . "<br>" . mysqli_error($db);
       echo "<script> alert ('เข้าสู่ระบบไม่สำเร็จ เนื่องจากผู้ดูแลไม่ได้เพิ่มสมาชิกเข้าร่วมทำงาน User กับ รหัสผ่านไม่ตรงกัน หรือไม่ได้อยู่ในระบบ จะต้องขอ Admin เป็นคนมีสิทธิ์เพิ่มสมาชิกด้วยนะครับผม'); </script> ";
