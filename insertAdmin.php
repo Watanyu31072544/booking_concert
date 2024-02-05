55fe83271adb8b8b7072b16ef97bc592cb88cb3d
Watanyu31072544 <kh.watanyu_st@tni.ac.th>
2024-02-05 10:17:31 +0700

    Update insertAdmin.php

--- insertAdmin.php
+++ insertAdmin.php
@@ -8,6 +8,17 @@
     $ad_role = $_POST['ad_role'];
     $ad_password = $_POST['ad_password'];
 
+    $check = "SELECT * FROM admin where ad_fullname = '$ad_fullname' or ad_username = '$ad_username' or ad_role = '$ad_role' or ad_password = '$ad_password'";
+
+    $result1 = mysqli_query($db, $check) or die(mysqli_error($db));
+    $num=mysqli_num_rows($result1);
+ 
+    if($num > 0)
+    {
+    echo "<script> alert('ข้อมูลองผู้ดูแลซ้ำไป กรุณาเพิ่มสมาชิกใหม่อีกครั้ง !'); </script> ";
+    echo "<script> window.location='formAddAdmin.php'; </script>";
+    }else{
+
     $sql = "INSERT INTO admin(ad_fullname, ad_username, ad_role, ad_password) value('$ad_fullname','$ad_username','$ad_role','$ad_password')";
     $result = mysqli_query($db,$sql);
     if($result){
@@ -19,5 +30,5 @@
     echo "<script> alert ('เพิ่มข้อมูลของผู้ดูแลไม่สำเร็จ'); </script> ";
     }
     mysqli_close($db);
-
+  }
   ?>
