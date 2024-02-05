20b90efa2fe318dfadd59f312f8e6407071e2f0c
Watanyu31072544 <kh.watanyu_st@tni.ac.th>
2024-02-05 15:10:40 +0700

    Update deleteMember.php

--- deleteMember.php
+++ deleteMember.php
@@ -2,17 +2,17 @@
 
 <?php
     include('dbconnect.php');
-
+    //เช็ครหัส ID ของลูกค้า ว่าได้รับรหัส ID ของลูกค้าหรือยัง
     $m_id = $_GET['m_id'];
-
+    //ถ้ารับค่ารหัส ID ของลูกค้า
     if(isset($_GET['member'])){
-    $sql = "DELETE FROM member WHERE m_id=".$_GET['member'];
+    $sql = "DELETE FROM member WHERE m_id=".$_GET['member'];//เมื่อเจอพบแล้ว ให้ Admin สามารถลบข้อมูลของลูกค้าออกจากระบบฐานข้อมูล
     }
     $result = mysqli_query($db, $sql);
-
+    //ถ้าเจอ Admin สามารถลบข้อมูลของลูกค้าได้
     if ($result) {
     echo "<script> alert('ลบข้อมูลของสมาชิกเรียบร้อยแล้วครับ'); </script>";
-    echo "<script> window.location='ManageMember.php'; </script>";
+    echo "<script> window.location='ManageMember.php'; </script>";//เมื่อลบข้อมูลของลูกค้าแล้ว ข้อความแจ้งเตือนขึ้นมา จากนั้นกลับมาหน้าจัดการข้อมูลของลูกค้าได้
     } else {
     echo "ไม่สามารถลบได้ หรือ มีข้อผิดพลาดเกิดขึ้น";
     }
