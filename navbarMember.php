e1fe2724443333e4f39d7a7f2c9ba3cf9c30c2ab
Watanyu31072544 <kh.watanyu_st@tni.ac.th>
2024-02-06 10:07:25 +0700

    Update navbarMember.php

--- navbarMember.php
+++ navbarMember.php
@@ -1,18 +1,21 @@
 <?php
 require("dbconnect.php");
 ?>
+<!-- Navbar ของระบบจองตั๋วคอนเสิร์ต สำหรับ ลูกค้า -->
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="PageBookingConcert.php">        
         <div class="sidebar-brand-text">Booking Concert</div>
     </a>
     <hr class="sidebar-divider my-0">          
     <li class="nav-item active">
+        <!-- หน้าดูข้อมูลของคอนเสิร์ต ลูกค้าสามารถเข้ามาดูข้อมูลของคอนเสิร์ตได้ -->
         <a class="nav-link" href="PageDataConcert.php">
             <i class="fa-solid fa-info"></i>
             <span>ข้อมูลคอนเสิร์ต</span>
         </a>
     </li>
     <li class="nav-item active">
+        <!-- หน้าจองตั๋วคอนเสิร์ต ลูกค้าสามารถเข้ามาจองที่นั่งของคอนเสิร์ตได้ -->
         <a class="nav-link" href="PageBookingConcert.php">
             <i class="fa-solid fa-ticket"></i>
             <span>จองตั๋วคอนเสิร์ต</span>
@@ -20,6 +23,7 @@ require("dbconnect.php");
     </li>
     <hr class="sidebar-divider my-0">
     <li class="nav-item active">
+        <!-- หน้าประวัติจองตั๋วคอนเสิร์ต ลูกค้าสามารถเข้ามาดูประวัติจองตั๋วคอนเสิร์ตได้ -->
         <a class="nav-link" href="PageHistoryBookingConcert.php">
             <i class="fa-regular fa-clock"></i>
             <span>ประวัติจองตั๋วคอนเสิร์ต</span>
@@ -27,12 +31,14 @@ require("dbconnect.php");
     </li>
     <hr class="sidebar-divider my-0">
     <li class="nav-item active">
+        <!-- หน้าโปรไฟล์ของลูกค้า ลูกค้าสามารถเข้ามาดูโปรไฟล์ของลูกค้าเองได้ -->
         <a class="nav-link" href="ProFileMember.php">
             <i class="fa-solid fa-user"></i>
             <span>โปรไฟล์</span>
         </a>
     </li>
     <li class="nav-item active">
+        <!-- เมื่อใช้งานเสร็จแล้ว ลูกค้าสามารถออกจากระบบได้ -->
         <a class="nav-link" href="formLoginMember.php">
             <i class="fa-solid fa-right-to-bracket"></i>
             <span>ออกจากระบบ</span>
