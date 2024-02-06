5663fdebabd973474465fe7de81d6c6304ed9607
Watanyu31072544 <kh.watanyu_st@tni.ac.th>
2024-02-06 08:51:11 +0700

    Update insertAdmin.php

--- insertAdmin.php
+++ insertAdmin.php
@@ -7,18 +7,18 @@
     $ad_username = $_POST['ad_username'];
     $ad_role = $_POST['ad_role'];
     $ad_password = $_POST['ad_password'];
-
+    //เช็คข้อมูลซ้ำที่อยู่ในฐานข้อมูลของผู้ดูแล
     $check = "SELECT * FROM admin where ad_fullname = '$ad_fullname' and ad_username = '$ad_username' and ad_role = '$ad_role' and ad_password = '$ad_password'";
 
     $result1 = mysqli_query($db, $check) or die(mysqli_error($db));
     $num=mysqli_num_rows($result1);
- 
+    //มีข้อมูลซ้ำกัน จะต้องกลับไปเพิ่มสมาชิกผู้ดูแลใหม่อีกครั้ง
     if($num > 0)
     {
     echo "<script> alert('ข้อมูลของผู้ดูแลซ้ำไป กรุณาเพิ่มสมาชิกของผู้ดูแลใหม่อีกครั้ง !'); </script> ";
     echo "<script> window.location='formAddAdmin.php'; </script>";
     }else{
-
+    //ข้อมูลไม่มีซ้ำ สามารถเพิ่มสมาชิกผู้ดูแลได้
     $sql = "INSERT INTO admin(ad_fullname, ad_username, ad_role, ad_password) value('$ad_fullname','$ad_username','$ad_role','$ad_password')";
     $result = mysqli_query($db,$sql);
     if($result){
@@ -27,7 +27,7 @@
     }
     else{
     echo "Error:" . $sql . "<br>" . mysqli_error($db);
-    echo "<script> alert ('เพิ่มข้อมูลของผู้ดูแลไม่สำเร็จ'); </script> ";
+    echo "<script> alert ('เพิ่มข้อมูลของผู้ดูแลไม่สำเร็จ เนื่องจากไม่ได้ฐานตารางข้อมูล'); </script> ";//ไม่อยู่ในฐานข้อมูล เนื่องจากไม่ได้สร้างฐานข้อมูล หรือมีข้อผิดพลาด
     }
     mysqli_close($db);
   }
