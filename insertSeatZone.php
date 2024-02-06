ddd52889f4d2d27d9260683e083c75450d1fe14a
Watanyu31072544 <kh.watanyu_st@tni.ac.th>
2024-02-06 08:51:29 +0700

    Update insertSeatZone.php

--- insertSeatZone.php
+++ insertSeatZone.php
@@ -5,18 +5,18 @@
     //ประกาศตัวแปรรับค่าจากฟอร์ม
     $s_zone = $_POST['s_zone'];
     $s_price = $_POST['s_price'];
-
+    //เช็คข้อมูลซ้ำที่อยู่ในฐานข้อมูลของโซนที่นั่ง
     $check = "SELECT * FROM seat_zone where s_zone = '$s_zone' and s_price = '$s_price'";
 
     $result1 = mysqli_query($db, $check) or die(mysqli_error($db));
     $num=mysqli_num_rows($result1);
- 
+    //มีข้อมูลซ้ำกัน จะต้องกลับไปเพิ่มโซนที่นั่งใหม่อีกครั้ง
     if($num > 0)
     {
     echo "<script> alert('คุณเพิ่งเพิ่มโซนที่นั่งไปแล้ว กรุณาเพิ่มชื่อโซนที่นั่งใหม่อีกครั้ง !'); </script> ";
     echo "<script> window.location='formAddSeatZone.php'; </script>";
     }else{
-
+    //ข้อมูลไม่มีซ้ำ สามารถเพิ่มโซนที่นั่งได้
     $sql = "INSERT INTO seat_zone(s_zone, s_price) value('$s_zone','$s_price')";
     $result = mysqli_query($db,$sql);
     if($result){
@@ -25,7 +25,7 @@
     }
     else{
       echo "Error:" . $sql . "<br>" . mysqli_error($db);
-      echo "<script> alert ('เพิ่มข้อมูลโซนที่นั่งไม่สำเร็จ'); </script> ";
+      echo "<script> alert ('เพิ่มข้อมูลโซนที่นั่งไม่สำเร็จ เนื่องจากไม่ได้ฐานตารางข้อมูล'); </script> ";//ไม่อยู่ในฐานข้อมูล เนื่องจากไม่ได้สร้างฐานข้อมูล หรือมีข้อผิดพลาด
     }
     mysqli_close($db);
     }
