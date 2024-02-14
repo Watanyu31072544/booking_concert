804c83ba1db0f19ed4d9c7a134c324b3b57d93e3
Watanyu31072544 <kh.watanyu_st@tni.ac.th>
2024-02-14 11:41:41 +0700

    Create PageReportBookingConcertOfMemberByAdmin.php

---
+++ PageReportBookingConcertOfMemberByAdmin.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>จัดการข้อมูลของลูกค้า</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="fontawesome/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet" type="text/css">
+
+</head>
+
+<body id="page-top">
+    <!-- Page Wrapper -->
+    <div id="wrapper" style="width: auto; height: auto;">
+        <?php
+        include('navbarManage.php');
+        ?>
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+            <!-- Main Content -->
+            <div id="content" style="background-color: #B5DE0C;">
+                <?php
+                include('topbarManage.php');
+                ?>
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+                    <!-- Page Heading -->
+                    <?php
+                    include('connect.php');
+                    $sql = "select * from booking";
+                    if(!$result = $db -> query($sql)){
+                        die($db -> error);
+                    }
+                    $countResult = $result -> num_rows; //นับจำนวนแถวจากคำสั่ง $sql
+                    ?>
+                    <div class="card-body">
+                            <div class="row">
+                                <h1 class="h3 mb-0 text-color-800" id="website">รายงานของลูกค้าที่จองไป </h1>
+                            </div>
+                            <br>
+                            <div class="row">
+                                <div class="col-xl-3 col-md-6 mb-4">
+                                    <div class="card border-left-primary shadow h-100 py-2">
+                                        <div class="card-body">
+                                            <div class="row no-gutters align-items-center">
+                                                <div class="col mr-2">
+                                                    <div class="h5 mb-0 text-color-800" id="website">
+                                                        จำนวนที่จอง</div>
+                                                    <div class="h5 mb-0 text-color-800" id="website"><?php echo $countResult?> รายการ</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <?php
+                                    $sql1 = "select sum(s_price) AS sum_price from booking";
+                                    $query = mysqli_query($db,$sql1);
+                                    $total = mysqli_fetch_assoc($query);
+                                ?>
+                                <div class="col-xl-3 col-md-6 mb-4">
+                                    <div class="card border-left-primary shadow h-100 py-2">
+                                        <div class="card-body">
+                                            <div class="row no-gutters align-items-center">
+                                                <div class="col mr-2">
+                                                    <div class="h5 mb-0 text-color-800" id="website">
+                                                        รวมเงินทั้งหมดจากลูกค้า</div>
+                                                    <div class="h5 mb-0 text-color-800" id="website">฿ <?php echo number_format($total['sum_price'],0); ?></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <?php
+                                    $sql2 = "select * from booking group by m_fullname";
+                                    if(!$result1 = $db -> query($sql2)){
+                                        die($db -> error);
+                                    }
+                                    $countResult1 = $result1 -> num_rows;
+                                ?>
+                                <div class="col-xl-3 col-md-6 mb-4">
+                                    <div class="card border-left-primary shadow h-100 py-2">
+                                        <div class="card-body">
+                                            <div class="row no-gutters align-items-center">
+                                                <div class="col mr-2">
+                                                    <div class="h5 mb-0 text-color-800" id="website">
+                                                        จำนวนลูกค้าที่จอง</div>
+                                                    <div class="h5 mb-0 text-color-800" id="website"><?php echo $countResult1; ?> คน</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>    
+                    </div>             
+                </div>
+                <!-- /.container-fluid -->
+            </div>
+            <!-- End of Main Content -->
+        </div>
+        <!-- End of Content Wrapper -->
+    </div>
+    <!-- End of Page Wrapper -->
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
+    <!-- Bootstrap core JavaScript-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin-2.min.js"></script>
+    <!-- Page level plugins -->
+    <script src="vendor/datatables/jquery.dataTables.min.js"></script>
+    <script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
+    <!-- Page level custom scripts -->
+    <script src="js/demo/datatables-demo.js"></script>
+</body>
+
+</html>
