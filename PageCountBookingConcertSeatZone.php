768118393bfba580f6e0c3a777affacf6d5235bd
Watanyu31072544 <kh.watanyu_st@tni.ac.th>
2024-02-15 13:52:33 +0700

    Update

--- PageCountBookingConcertSeatZone.php
+++ PageCountBookingConcertSeatZone.php
@@ -74,7 +74,16 @@
                             <tbody class="text-color" align="center";>                            
                             <?php
                             include('connect.php');
-                                $sql = "select s_zone,count(s_zone) from booking where s_zone is not null and name = '".$concert['name']."'GROUP BY s_zone";
+                                $sql1 = "select s_zone,count(s_zone) from booking where s_zone is not null and name = '".$concert['name']."'GROUP BY s_zone";
+                                if(!$result1 = $db -> query($sql1)){
+                                    die($db -> error);
+                                }
+                                $countResult1 = $result1 -> num_rows; //นับจำนวนแถวจากคำสั่ง $sql
+                                //////////////////////////////////////
+                            ?>
+                            <?php
+                                include('connect.php');
+                                $sql = "select * from seat_zone";
                                 if(!$result = $db -> query($sql)){
                                     die($db -> error);
                                 }
@@ -97,14 +106,22 @@
                             ?>
                             <?php
                                 for($i=1; $i<=$countPageResult; $i++){
-                                    $booking = $result -> fetch_assoc();?>
+                                    $seat_zone = $result -> fetch_assoc();
+                                    $booking = $result1 -> fetch_assoc();?>
                                 <tr> <!-- แสดงตารางที่อยู่ในฐานข้อมูลของโซนที่นั่ง -->
-                                <td><?php echo $booking['s_zone']; ?></td>
-                                <td><?php echo 10;?></td>
-                                <td><?php echo 10-$booking['count(s_zone)'];?></td>
-                                <td><?php echo $booking['count(s_zone)'];?></td> <!-- สามารถเลือกโซนที่นั่งได้ตามที่ต้องการ -->
-                                
-                                <td><?php if($booking['count(s_zone)'] == 10){//พื้นที่โซนที่นั่งเต็มแล้ว
+                                <td><?php echo $seat_zone['s_zone']; ?></td>
+                                <td><?php echo $seat_zone['s_qty'];?></td>
+                                <td><?php if(isset($booking['count(s_zone)']) != null){
+                                    echo $seat_zone['s_qty']-$booking['count(s_zone)'];
+                                }else{
+                                    echo $seat_zone['s_qty'];
+                                } ?></td>
+                                <td><?php if(isset($booking['count(s_zone)']) != null){
+                                    echo $booking['count(s_zone)'];
+                                }else{
+                                    echo 0;
+                                } ?></td> <!-- สามารถเลือกโซนที่นั่งได้ตามที่ต้องการ -->
+                                <td><?php if((isset($booking['count(s_zone)']) - $seat_zone['s_qty']) == 0){//พื้นที่โซนที่นั่งเต็มแล้ว
                                     echo 'เต็มแล้ว';
                                 } else {//พื้นที่โซนที่นั่งว่างอยู่
                                     echo 'ว่างอยู่';
